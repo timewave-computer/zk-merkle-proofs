a208da614673f608055487e25b1f6d46ae11d644
jonas pauli <jonaspauli089@gmail.com>
2025-03-04 16:14:43 +0100

    purge repo for review

--- sp1/merkle-program/src/main.rs
+++
@@ -1,7 +0,0 @@
-#![no_main]
-sp1_zkvm::entrypoint!(main);
-
-/// the logic that is to be proven
-/// will likely call external functions, primarily verify_merkle_proof
-/// enable sp1 as a feature to use keccak precompile
-pub fn main() {}
