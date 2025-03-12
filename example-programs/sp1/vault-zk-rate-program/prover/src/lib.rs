7f67d104d66cdaa0d11cea0028d8b563cd9c1df8
jonas pauli <jonaspauli089@gmail.com>
2025-03-12 17:29:22 +0100

    minor fixes

--- example-programs/sp1/vault-zk-rate-program/prover/src/lib.rs
+++ example-programs/sp1/vault-zk-rate-program/prover/src/lib.rs
@@ -1,21 +1,12 @@
 use std::time::Instant;
-pub const MERKLE_ELF: &[u8] = include_elf!("cross-chain-message-builder-guest");
-use cross_chain_message_builder_types::MessageBuilderProgramInput;
-/// entry point for the proving service
-/// this function will be used to prove the merkle-program execution
-/// the merkle-program will use verify_merkle_proof to verify one or more opening(s)
+pub const MERKLE_ELF: &[u8] = include_elf!("vault-zk-rate-guest");
 use sp1_sdk::{include_elf, ProverClient, SP1Stdin};
-pub fn prove() {
-    
-}
+pub fn prove() {}
 
 #[cfg(test)]
 mod tests {
     use crate::prove;
-    use cross_chain_message_builder_types::MessageBuilderProgramInput;
 
     #[tokio::test]
-    async fn test_generate_zk_rate_proof() {
-
-    }
+    async fn test_generate_zk_transfer_proof() {}
 }
