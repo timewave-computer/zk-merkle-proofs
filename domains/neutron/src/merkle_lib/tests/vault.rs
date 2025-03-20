87a42e089d196459b6d633ede614727afb89fed0
jonas pauli <jonaspauli089@gmail.com>
2025-03-20 01:45:04 +0100

    verify all proofs in clear

--- domains/neutron/src/merkle_lib/tests/vault.rs
+++ domains/neutron/src/merkle_lib/tests/vault.rs
@@ -6,13 +6,12 @@ mod tests {
         merkle_lib::{
             tests::defaults::{
                 read_pion_1_default_account_address, read_pion_1_vault_contract_address,
-                read_rpc_url, read_test_vector_height,
+                read_rpc_url, read_test_vector_height, read_test_vector_merkle_root,
             },
-            types::MerkleProverNeutron,
+            types::{MerkleProverNeutron, NeutronMerkleProof},
         },
     };
-    use common::merkle::types::MerkleProver;
-
+    use common::merkle::types::{MerkleProver, MerkleVerifiable};
     #[tokio::test]
     pub async fn test_get_neutron_pion_vault_contract_balance_merkle_proof() {
         let rpc_url = read_rpc_url();
@@ -22,9 +21,11 @@ mod tests {
             &read_pion_1_default_account_address(),
             &read_pion_1_vault_contract_address(),
         );
-        let _proofs = prover
+        let proofs = prover
             .get_merkle_proof_from_rpc(&neutron_key.serialize(), "", read_test_vector_height())
             .await;
+        let neutron_proof: NeutronMerkleProof = serde_json::from_slice(&proofs).unwrap();
+        neutron_proof.verify(&base64::decode(read_test_vector_merkle_root()).unwrap());
     }
 
     #[tokio::test]
@@ -33,8 +34,10 @@ mod tests {
         let prover = MerkleProverNeutron { rpc_url };
         let neutron_key =
             NeutronKey::new_wasm_stored_value("shares", &read_pion_1_vault_contract_address());
-        let _proofs = prover
+        let proofs = prover
             .get_merkle_proof_from_rpc(&neutron_key.serialize(), "", read_test_vector_height())
             .await;
+        let neutron_proof: NeutronMerkleProof = serde_json::from_slice(&proofs).unwrap();
+        neutron_proof.verify(&base64::decode(read_test_vector_merkle_root()).unwrap());
     }
 }
