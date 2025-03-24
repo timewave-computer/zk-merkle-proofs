36dfb1b58d56548b40c78740a0b8748af81787f9
jonas pauli <jonaspauli089@gmail.com>
2025-03-24 14:47:08 +0100

    remove prover-utils for now

--- prover-utils/src/merkle/types.rs
+++
@@ -1,8 +0,0 @@
-use ethereum::merkle_lib::types::EthereumMerkleProof;
-use serde::{Deserialize, Serialize};
-
-/// Circuit input - multiple proofs for multiple domains
-#[derive(Serialize, Deserialize, Clone, Debug)]
-pub struct MerkleProofInput {
-    pub ethereum_proofs: Vec<EthereumMerkleProof>,
-}
