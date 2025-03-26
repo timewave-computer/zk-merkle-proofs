1d655b328fe054779f6e129cca6ae7e9b29c69dd
jonas pauli <jonaspauli089@gmail.com>
2025-03-26 16:59:51 +0100

    use include bytes macro for test

--- domains/ethereum/src/merkle_lib/tests/merkle.rs
+++ domains/ethereum/src/merkle_lib/tests/merkle.rs
@@ -8,10 +8,7 @@ mod tests {
 
     #[tokio::test]
     async fn test_verify_account_proof() {
-        let block_root: Vec<u8> = vec![
-            118, 40, 34, 157, 173, 69, 97, 39, 178, 229, 149, 0, 102, 49, 6, 23, 193, 155, 138,
-            107, 206, 30, 43, 33, 235, 240, 232, 137, 63, 146, 55, 211,
-        ];
+        let block_root: Vec<u8> = include_bytes!("data/block_root.bin").to_vec();
         let eth_proof: EthereumMerkleProof =
             serde_json::from_slice(&get_test_vector_eth_account_proof()).unwrap();
         assert!(eth_proof.verify(&block_root));
@@ -19,10 +16,7 @@ mod tests {
 
     #[tokio::test]
     async fn test_verify_storage_proof() {
-        let account_root: Vec<u8> = vec![
-            220, 247, 143, 8, 23, 115, 27, 234, 201, 222, 6, 127, 108, 199, 142, 189, 143, 170,
-            229, 114, 198, 145, 98, 162, 74, 233, 250, 165, 112, 210, 250, 206,
-        ];
+        let account_root: Vec<u8> = include_bytes!("data/account_root.bin").to_vec();
         let eth_proof: EthereumMerkleProof =
             serde_json::from_slice(&get_test_vector_eth_storage_proof()).unwrap();
         assert!(eth_proof.verify(&account_root));
