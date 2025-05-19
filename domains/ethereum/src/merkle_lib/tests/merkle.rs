88a5d6081869bb45b40594c6a7eefbccbf89f631
jonas pauli <jonaspauli089@gmail.com>
2025-05-19 13:19:39 +0200

    log the account object

--- domains/ethereum/src/merkle_lib/tests/merkle.rs
+++ domains/ethereum/src/merkle_lib/tests/merkle.rs
@@ -8,15 +8,17 @@ mod tests {
         types::{rlp_decode_account, EthereumMerkleProof},
     };
     use common::merkle::types::MerkleVerifiable;
+    use tracing::info;
 
     #[tokio::test]
     async fn test_verify_account_proof() {
+        tracing_subscriber::fmt::init();
         let block_root: Vec<u8> = get_test_vector_eth_block_root();
         let eth_proof: EthereumMerkleProof =
             serde_json::from_slice(&get_test_vector_eth_account_proof()).unwrap();
         let account_rlp = eth_proof.value.clone();
         let account_decoded = rlp_decode_account(&account_rlp).expect("Failed to decode account");
-        println!("Account Decoded: {:?}", account_decoded);
+        info!("Account Decoded: {:?}", account_decoded);
         assert!(eth_proof.verify(&block_root).unwrap());
     }
 
